eb93b3e15c207ada640192fbb63a136f364fb49a
IgCand22 <icand32@gmail.com>
2025-09-20 12:31:47 -0300

    Testando commit

--- Ex1028.java
+++ Ex1028.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex1028 {
+    public static class Recursivo{
+        public int verificar(int maior, int menor){
+            if(menor <= 0){
+                return maior;
+            }
+            return verificar(menor, maior%menor);
+        }
+    }
+
+    public static class Vetor{
+        int[] vetor;
+        int capacidade;
+        int n_elementos;
+
+        public Vetor(int capacidade){
+            this.capacidade = capacidade;
+            this.vetor = new int[capacidade];
+            n_elementos = 0;
+        }
+
+        public boolean vetorCheio(){
+            return n_elementos == capacidade;
+        }
+
+        public void addElementoFinal(int elemento){
+            if (vetorCheio()){
+                System.out.println("Vetor está na capacidade máxima");
+                return;
+            }
+            vetor[n_elementos] = elemento;
+            n_elementos++;
+        }
+
+        public void imprimirVetor(){
+            for(int i = 0; i < n_elementos; i++){
+                System.out.println(vetor[i]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Recursivo n = new Recursivo();
+        int maior;
+        int menor;
+        Scanner scan = new Scanner(System.in);
+        Vetor vetor = new Vetor(scan.nextInt());
+        for(int i = 0; i < vetor.capacidade; i++){
+            int n1 = scan.nextInt();
+            int n2 = scan.nextInt();
+
+            if (n2 > n1){
+                menor = n1;
+                maior = n2;
+            }
+            maior = n1;
+            menor = n2;
+
+            vetor.addElementoFinal(n.verificar(maior, menor));
+        }
+        vetor.imprimirVetor();
+    }
 }
--- Ex1029.java
+++ Ex1029.java
@@ -1,4 +1,69 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex1029 {
+    public static class FibonnaciRecursivo{
+        private int valorCalculado;
+        private int calls;
+
+       public int getCalls(){
+            return calls-1;
+        }
+
+        public int seqFib(int valor){
+            calls++;
+            if (valor == 0){
+                return 0;
+            } else if (valor == 1) {
+                return 1;
+            }
+            return seqFib(valor-1) + seqFib(valor-2);
+
+        }
+    }
+
+    public static class Vetor{
+        int[] vetor;
+        int capacidade;
+        int n_elementos;
+
+        public Vetor(int capacidade){
+            this.capacidade = capacidade;
+            this.vetor = new int[capacidade];
+            n_elementos = 0;
+        }
+
+        public boolean vetorCheio(){
+            return n_elementos == capacidade;
+        }
+
+        public void addElementoFinal(int elemento){
+            if (vetorCheio()){
+                System.out.println("Vetor está na capacidade máxima");
+                return;
+            }
+            vetor[n_elementos] = elemento;
+            n_elementos++;
+        }
+
+        public void imprimirVetor(){
+            for(int i = 0; i < capacidade/3; i++){
+                System.out.println("fib(" + vetor[i] + ")" + " " + vetor[i+1] + " " + "calls" + vetor[i+2]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int qtdNum = scan.nextInt();
+        Vetor vetor = new Vetor(qtdNum*3);
+        for (int i = 0; i < qtdNum; i++) {
+            FibonnaciRecursivo fib = new FibonnaciRecursivo();
+            int numero = scan.nextInt();
+            vetor.addElementoFinal(numero);
+            vetor.addElementoFinal(fib.seqFib(numero));
+            vetor.addElementoFinal(fib.getCalls());
+        }
+        vetor.imprimirVetor();
+    }
 }
